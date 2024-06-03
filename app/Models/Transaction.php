96f77fe3dabf5e6b6eadbf325bc68490c794d457
Fathurrahman <10190031@gmail.com>
2024-06-03 15:40:32 +0700

    Menambahkan accessor

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
 
 class Transaction extends Model
 {
@@ -21,4 +23,11 @@ class Transaction extends Model
     {
         return $this->belongsTo(Bank::class);
     }
+
+    protected function createdAt(): Attribute
+     {
+         return Attribute::make(
+             get: fn ($value) => Carbon::parse($value)->format('d-M-Y H:i:s'),
+         );
+     }
 }
