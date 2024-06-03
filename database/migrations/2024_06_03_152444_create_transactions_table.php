bd8c435433edbc7917381b762adc83f94b31f0a3
Fathurrahman <10190031@gmail.com>
2024-06-03 15:31:43 +0700

    create models and migrations

---
+++ database/migrations/2024_06_03_152444_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('bank_id');
+            $table->string('title');
+            $table->bigInteger('amount');
+            $table->enum('status', ['income', 'expend']); 
+            $table->string('transaction_date');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
